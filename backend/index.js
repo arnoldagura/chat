90e9fbd1a04ac5419265cd76a9233f42698f4ffb
LAPTOP-UD506OH5\Arnold <arnold.agura@brank.as>
2023-04-19 14:32:09 +0800

    create BE and FE

---
+++ backend/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: true }));
+
+app.post("/authenticate", async (req, res) => {
+    const { username } = req.body;
+    // Get or create user on Chat Engine!
+    try {
+      const r = await axios.put(
+        "https://api.chatengine.io/users/",
+        { username: username, secret: username, first_name: username },
+        { headers: { "Private-Key": "650b31af-8676-4f1a-bb93-6a12c58683cf" } }
+      );
+      return res.status(r.status).json(r.data);
+    } catch (e) {
+      return res.status(e.response.status).json(e.response.data);
+    }
+  });
+
+app.listen(3001);
